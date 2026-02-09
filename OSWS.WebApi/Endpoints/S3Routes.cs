904ddb0e0e53bc558ba6085eaf4df2dcedd3f82f
lucasfth <online@lucashanson.dk>
2026-02-09 10:02:13 +0100

    Fix AOT warning

--- OSWS.WebApi/Endpoints/S3Routes.cs
+++ OSWS.WebApi/Endpoints/S3Routes.cs
@@ -12,60 +12,58 @@ public static class S3Routes
     public static IEndpointRouteBuilder MapS3Routes(this IEndpointRouteBuilder app)
     {
         // S3 GET - path-style routing for S3 compatibility: /{bucket}/{*key} and /{bucket}
-        var s3GetHandler = async (
-            [FromServices] IS3Get s3Get,
-            string bucket,
-            string? key,
-            [AsParameters] Params prms,
-            [AsParameters] S3Options s3Options,
-            HttpRequest httpRequest,
-            HttpResponse httpResponse,
-            [FromQuery] int retryOptions = DefaultRetryOptions,
-            [FromQuery] int timeoutOptionsMs = DefaultTimeoutOptionsMs,
-            CancellationToken cancellationToken = default
-        ) =>
-        {
-            return await s3Get.GetObject(
-                bucket,
-                key,
-                prms,
-                s3Options,
-                httpRequest,
-                httpResponse,
-                retryOptions,
-                timeoutOptionsMs,
-                cancellationToken
-            );
-        };
-
-        app.MapGet("/{bucket}/{*key}", s3GetHandler);
+        app.MapGet(
+            "/{bucket}/{*key}",
+            async (
+                [FromServices] IS3Get s3Get,
+                string bucket,
+                string? key,
+                [AsParameters] Params prms,
+                [AsParameters] S3Options s3Options,
+                HttpRequest httpRequest,
+                HttpResponse httpResponse,
+                [FromQuery] int retryOptions = DefaultRetryOptions,
+                [FromQuery] int timeoutOptionsMs = DefaultTimeoutOptionsMs,
+                CancellationToken cancellationToken = default
+            ) =>
+                await s3Get.GetObject(
+                    bucket,
+                    key,
+                    prms,
+                    s3Options,
+                    httpRequest,
+                    httpResponse,
+                    retryOptions,
+                    timeoutOptionsMs,
+                    cancellationToken
+                )
+        );
 
         // S3 PUT - path-style routing for S3 compatibility: /{bucket}/{*key}
-        var s3PutHandler = async (
-            [FromServices] IS3Put s3Put,
-            string bucket,
-            string? key,
-            [AsParameters] Params prms,
-            [AsParameters] S3Options s3Options,
-            HttpRequest httpRequest,
-            [FromQuery] int retryOptions = DefaultRetryOptions,
-            [FromQuery] int timeoutOptionsMs = DefaultTimeoutOptionsMs,
-            CancellationToken cancellationToken = default
-        ) =>
-        {
-            return await s3Put.PutObject(
-                bucket,
-                key,
-                prms,
-                s3Options,
-                httpRequest,
-                retryOptions,
-                timeoutOptionsMs,
-                cancellationToken
-            );
-        };
-
-        app.MapPut("/{bucket}/{*key}", s3PutHandler);
+        app.MapPut(
+            "/{bucket}/{*key}",
+            async (
+                [FromServices] IS3Put s3Put,
+                string bucket,
+                string? key,
+                [AsParameters] Params prms,
+                [AsParameters] S3Options s3Options,
+                HttpRequest httpRequest,
+                [FromQuery] int retryOptions = DefaultRetryOptions,
+                [FromQuery] int timeoutOptionsMs = DefaultTimeoutOptionsMs,
+                CancellationToken cancellationToken = default
+            ) =>
+                await s3Put.PutObject(
+                    bucket,
+                    key,
+                    prms,
+                    s3Options,
+                    httpRequest,
+                    retryOptions,
+                    timeoutOptionsMs,
+                    cancellationToken
+                )
+        );
 
         return app;
     }

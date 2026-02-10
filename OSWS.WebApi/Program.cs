2ac155b066c313c9dbc8c5c2db90e432bb2fca53
Andreas Trøstrup <andreastrstrup@gmail.com>
2026-02-10 15:05:00 +0100

    Add db setup

--- OSWS.WebApi/Program.cs
+++ OSWS.WebApi/Program.cs
@@ -2,6 +2,7 @@ using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Microsoft.EntityFrameworkCore;
+using OSWS.Common.Configuration;
 using OSWS.KeyManager.Persistence;
 using OSWS.Library;
 using OSWS.ParquetSolver;
@@ -16,15 +17,19 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     // options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
+// Configure DatabaseSettings from appsettings.json
+
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddDbContext<OswsContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("OswsContext"))
+);
+
 var r2Endpoint =
     Environment.GetEnvironmentVariable("R2_ENDPOINT")
     ?? "https://f2087fdd677819f9e9095e98c80537b9.r2.cloudflarestorage.com";
 var r2AccessKey = Environment.GetEnvironmentVariable("R2_ACCESS_KEY_ID") ?? "";
 var r2SecretKey = Environment.GetEnvironmentVariable("R2_SECRET_ACCESS_KEY") ?? "";
 var r2Region = Environment.GetEnvironmentVariable("R2_REGION") ?? "auto"; // can be any string for S3-compatible providers
-
-var databaseSettings;
-builder.Services.AddDbContext<OswsContext>(opts => opts.UseNpgsql());
 
 builder.Services.AddTransient<IS3Get, S3Get>();
 builder.Services.AddTransient<IS3Put, S3Put>();
